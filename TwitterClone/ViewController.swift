ceba666fe354dcdf9cb7c3849396b1761ac699d7
bodzio2k <bodzio2k@live.com>
2020-03-12 20:08:12 +0100

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Krzysztof Podolak on 12/03/2020.
+//  Copyright © 2020 Krzysztof Podolak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
